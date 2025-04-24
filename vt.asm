67f5802e20977145fb2f0e6f32c3095f59cc60b2
feifly99 <148175317+feifly99@users.noreply.github.com>
2025-04-24 19:49:36 +0800

    Update vt.asm

--- vt.asm
+++ vt.asm
@@ -1,12 +1,169 @@
+EXTERN KeBugCheckExAddress: QWORD
+
 .code
-
-	; Software can determine a processor��s physical-address width by executing CPUID with 80000008H in EAX. 
-	; The physical-address width is returned in bits 7:0 of EAX.
-
-	__vasm__checkCPUID PROC
-		mov eax, 80000008h
-		cpuid
+	
+	;some initialization before executing VMXON
+	__vsm__NOP PROC
+		nop
+		nop
+		nop
+		nop
+		nop
 		ret
-	__vasm__checkCPUID ENDP
+	__vsm__NOP ENDP
+
+	__vasm__isVMXOperationsSupported PROC
+		mov eax, 1
+		cpuid
+		shr ecx, 5
+		and ecx, 1
+		mov rax, rcx
+		ret
+	__vasm__isVMXOperationsSupported ENDP
+
+	__vasm__setCR4VMXEBit PROC
+		mov rax, cr4
+		mov ebx, 1
+		shl rbx, 13
+		or rax, rbx
+		mov cr4, rax
+		ret
+	__vasm__setCR4VMXEBit ENDP
+
+	__vsm__guestEntry PROC		
+		mov rax, qword ptr [0]
+		;page-fault
+		ret
+	__vsm__guestEntry ENDP
+
+	__vsm__hostEntry PROC
+		mov ecx, 0DEADBEEFh
+		xor edx, edx
+		xor r8, r8
+		xor r9, r9
+		mov rsi, KeBugCheckExAddress
+		call rsi
+		;蓝屏DEADBEEF
+		ret
+	__vsm__hostEntry ENDP
+
+	;GUEST_STATE_AREA_FIELDS_INITIALIZATION
+	__vsm__getCR0 PROC
+		mov rax, cr0
+		ret
+	__vsm__getCR0 ENDP
+
+	__vsm__getCR3 PROC
+		mov rax, cr3
+		ret
+	__vsm__getCR3 ENDP		
+
+	__vsm__getCR4 PROC
+		mov rax, cr4
+		ret
+	__vsm__getCR4 ENDP
+
+	__vsm__restoreCR4 PROC
+		mov rax, cr4
+		mov ecx, 1
+		shl ecx, 13
+		not ecx
+		and rax, rcx
+		mov cr4, rax
+		ret
+	__vsm__restoreCR4 ENDP
+		
+	__vsm__getDR7 PROC
+		mov rax, dr7
+		ret
+	__vsm__getDR7 ENDP
+		
+	__vsm__getRSP PROC
+		mov rax, rsp
+		ret
+	__vsm__getRSP ENDP
+		
+	__vsm__getRIP PROC
+		call _temp
+	_temp:
+		pop rax
+		sub rax, 5 ;魔法数字5：call指令短跳的长度是5个字节！
+		ret
+	__vsm__getRIP ENDP
+	
+	__vsm__getRFLAGS PROC
+		pushfq
+		pop rax
+		ret
+	__vsm__getRFLAGS ENDP
+	
+	;selectors
+	__vsm__getCS PROC
+		mov eax, cs
+		ret
+	__vsm__getCS ENDP
+		
+	__vsm__getSS PROC
+		mov eax, ss
+		ret
+	__vsm__getSS ENDP
+		
+	__vsm__getDS PROC
+		mov eax, ds
+		ret
+	__vsm__getDS ENDP
+		
+	__vsm__getES PROC
+		mov eax, es
+		ret
+	__vsm__getES ENDP
+		
+	__vsm__getFS PROC
+		mov eax, fs
+		ret
+	__vsm__getFS ENDP
+		
+	__vsm__getGS PROC
+		mov eax, gs
+		ret
+	__vsm__getGS ENDP
+		
+	__vsm__getLDTR PROC
+		sldt eax
+		ret
+	__vsm__getLDTR ENDP
+		
+	__vsm__getTR PROC
+		str eax
+		ret
+	__vsm__getTR ENDP
+
+	__vsm__getGDTbase PROC
+		LOCAL gdtr[10]:BYTE
+		sgdt gdtr
+		mov rax, qword ptr gdtr[2]
+		ret
+	__vsm__getGDTbase ENDP
+
+	__vsm__getGDTlimit PROC
+		LOCAL gdtr[10]:BYTE
+		sgdt gdtr
+		mov	 ax, word ptr gdtr[0]
+		ret
+	__vsm__getGDTlimit ENDP
+
+	__vsm__getIDTbase PROC
+		LOCAL idtr[10]:BYTE
+		sidt idtr
+		mov	rax, qword ptr idtr[2]
+		ret
+	__vsm__getIDTbase ENDP
+
+	__vsm__getIDTlimit PROC
+		LOCAL idtr[10]:BYTE
+		sidt idtr
+		mov	ax, word ptr idtr[0]
+		ret
+	__vsm__getIDTlimit ENDP
 
 END
