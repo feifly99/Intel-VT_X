01d49438597566e3fae93cf258ea805bfac1831d
feifly99 <148175317+feifly99@users.noreply.github.com>
2025-05-23 18:59:03 +0800

    Update entry.c

--- entry.c
+++ entry.c
@@ -5,17 +5,7 @@
 #pragma warning(disable: 6387)
 #pragma warning(disable: 6011)
 #pragma warning(disable: 4996)
-
-ULONG_PTR debugPrint = 0;
-
-ULONG_PTR bugCheck = 0;
-ULONG64 GlobalDebugWindow = 0;
-ULONG64 IS_CAPABILITY_MODE = 0;
-
-ULONG64 DRIVER_RSP = 0;
-ULONG64 DRIVER_RIP = 0;
-
-ULONG64 tempFLAGS = 0;
+#pragma warning(disable: 6386)
 
 ULONG GUEST_FS_BASE_INDEX = GUEST_FS_BASE_ADDRESS;
 ULONG GUEST_CR0_INDEX = GUEST_CR0;
@@ -24,72 +14,26 @@ ULONG GUEST_CR4_INDEX = GUEST_CR4;
 ULONG GUEST_RFLAGS_INDEX = GUEST_RFLAGS;
 ULONG GUEST_RSP_INDEX = GUEST_RSP;
 ULONG GUEST_RIP_INDEX = GUEST_RIP;
-ULONG HOST_RIP_INDEX =  HOST_RIP;
-ULONG HOST_RSP_INDEX =  HOST_RSP;
+ULONG HOST_RIP_INDEX = HOST_RIP;
+ULONG HOST_RSP_INDEX = HOST_RSP;
 ULONG VM_EXIT_REASON_INDEX = VEIF_EXIT_REASON;
 ULONG VM_EXIT_QUALIFICATION_INDEX = VEIF_EXIT_QUALIFICATION;
 ULONG VM_INSTRUCTION_LENGTH_INDEX = VEIF_VM_EXIT_INSTRUCTION_LENGTH;
 
-ULONG64 __CR0	 = 0;
-ULONG64 __CR3	 = 0;
-ULONG64 __CR4	 = 0;
-ULONG64 __RAX	 = 0;
-ULONG64 __RBX	 = 0;
-ULONG64 __RCX	 = 0;
-ULONG64 __RDX	 = 0;
-ULONG64 __RSI	 = 0;
-ULONG64 __RDI	 = 0;
-ULONG64 __RSP	 = 0;
-ULONG64 __RBP	 = 0;
-ULONG64 __RIP	 = 0;
-ULONG64 __RFLAGS = 0;
-ULONG64 __R8	 = 0;
-ULONG64 __R9	 = 0;
-ULONG64 __R10	 = 0;
-ULONG64 __R11	 = 0;
-ULONG64 __R12	 = 0;
-ULONG64 __R13	 = 0;
-ULONG64 __R14	 = 0;
-ULONG64 __R15	 = 0;
-USHORT EXIT_REASON = 0;
-ULONG INSTRUCTION_LENGTH = 0;
+PVCPU vCpu;
 
-CONST CHAR CPUID_STRING[50] = "CALL_CPUID";
-CONST CHAR RDMSR_STRING[50] = "CALL_RDMSR";
-CONST CHAR WRMSR_STRING[50] = "CALL_WRMSR";
+PULONG_PTR PER_CPU_REGS;
+
+ULONG_PTR bugCheck;
+
+ULONG totalCpuCount;
 
 #define print(s) \
 do\
 {\
 	ULONG64 ______________x = (s);\
-	DbgPrint("%s [%llu] [0x%p] <- \n", #s, ______________x, (PVOID)______________x);\
+	DbgPrint("%s -> [0x%p]\n", #s, (PVOID)______________x);\
 }while(0)
-
-PVCPU vCpus = NULL;
-KAFFINITY currentVMCSCpuIndex = 1;
-
-VOID driverUnload(PDRIVER_OBJECT driverObject)
-{
-	UNREFERENCED_PARAMETER(driverObject);
-	KeSetSystemAffinityThread((KAFFINITY)(1 << currentVMCSCpuIndex));
-	__vsm__vmoffSaveRegisters(); 
-	ULONG totalCpuCount = KeQueryActiveProcessorCount(NULL);
-	for (size_t j = 0; j < totalCpuCount; j++)
-	{
-		ExFreePool(vCpus[j].virtualGuestStack); 
-		vCpus[j].virtualGuestStack = NULL;
-		ExFreePool(vCpus[j].virtualHostStack); 
-		vCpus[j].virtualHostStack = NULL;
-		ExFreePool(vCpus[j].VMX_VMCS_REGION_VIRTUAL_KERNEL_ADDRESS); 
-		vCpus[j].VMX_VMCS_REGION_VIRTUAL_KERNEL_ADDRESS = NULL;
-		ExFreePool(vCpus[j].VMX_ON_REGION_VIRTUAL_KERNEL_ADDRESS); 
-		vCpus[j].VMX_ON_REGION_VIRTUAL_KERNEL_ADDRESS = NULL;
-	}
-	ExFreePool(vCpus);
-	vCpus = NULL;
-
-	return;
-}
 
 VOID getSegementRegisterAttributes(
 	IN SEGEMENT_TYPE type,
@@ -174,12 +118,12 @@ VOID getSegementRegisterAttributes(
 		ULONG_PTR currentSelectorGdtEntryPointer = GDTbase + index * 8; //8: magic number defined in INTEL® SDL
 		ULONG64 currentSelectorGdtEntry = *(ULONG64*)currentSelectorGdtEntryPointer;
 		sra->selector = (USHORT)selector;
-		print(currentSelectorGdtEntryPointer);
-		print(currentSelectorGdtEntry);
+		//print(currentSelectorGdtEntryPointer);
+		//print(currentSelectorGdtEntry);
 		sra->baseAddress = (*(ULONG_PTR*)(currentSelectorGdtEntryPointer + 8)) << 32;
-		print(sra->baseAddress);
+		//print(sra->baseAddress);
 		sra->baseAddress += (ULONG_PTR)(((currentSelectorGdtEntry >> 16) & 0xFFFFFFull) + ((currentSelectorGdtEntry & 0xFF00000000000000ull) >> 32));
-		print(sra->baseAddress);
+		//print(sra->baseAddress);
 		ULONG segementLimitTemp = (ULONG)((currentSelectorGdtEntry & 0xFFFFull));
 		if (currentSelectorGdtEntry & 0x000F000000000000ull)
 		{
@@ -198,60 +142,23 @@ VOID getSegementRegisterAttributes(
 	return;
 }
 
-NTSTATUS DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING regPath)
+ULONG_PTR v(ULONG_PTR arg)
 {
-	bugCheck = (ULONG_PTR)KeBugCheckEx;
-	debugPrint = (ULONG_PTR)DbgPrint;
+	UNREFERENCED_PARAMETER(arg);
 
-	UNREFERENCED_PARAMETER(regPath);
-	driverObject->DriverUnload = driverUnload;
-	ULONG totalCpuCount = KeQueryActiveProcessorCount(NULL);
-	vCpus = (PVCPU)ExAllocatePoolWithTag(NonPagedPool, totalCpuCount * sizeof(VCPU), 'z+aa');
-	RtlZeroMemory(vCpus, totalCpuCount * sizeof(VCPU));
-	SIZE_T regionSizeNeeded = 0x1000;
-	ULONG vmcsIdentifier = (ULONG)1;
-	//初始化虚拟CPU的属性
-	for (size_t j = 0; j < totalCpuCount; j++)
-	{
-		vCpus[j].currentCr4 = __vsm__getCR4();
-
-		vCpus[j].VMX_ON_REGION_VIRTUAL_KERNEL_ADDRESS = ExAllocatePoolWithTag(NonPagedPool, regionSizeNeeded, 'vmon');
-		RtlZeroMemory(vCpus[j].VMX_ON_REGION_VIRTUAL_KERNEL_ADDRESS, regionSizeNeeded);
-		PHYSICAL_ADDRESS tempVmxon = { 0 };
-		tempVmxon = MmGetPhysicalAddress(vCpus[j].VMX_ON_REGION_VIRTUAL_KERNEL_ADDRESS);
-		vCpus[j].VMX_ON_REGION_PHYSICAL_ADDRESS = tempVmxon.QuadPart;
-
-		vCpus[j].VMX_VMCS_REGION_VIRTUAL_KERNEL_ADDRESS = ExAllocatePoolWithTag(NonPagedPool, regionSizeNeeded, 'vmcs');
-		RtlZeroMemory(vCpus[j].VMX_VMCS_REGION_VIRTUAL_KERNEL_ADDRESS, regionSizeNeeded);
-		PHYSICAL_ADDRESS tempVmcsRegion = { 0 };
-		tempVmcsRegion = MmGetPhysicalAddress(vCpus[j].VMX_VMCS_REGION_VIRTUAL_KERNEL_ADDRESS);
-		vCpus[j].VMX_VMCS_REGION_PHYSICAL_ADDRESS = tempVmcsRegion.QuadPart;
-
-		vCpus[j].virtualGuestStackSize = 0x101000;
-		vCpus[j].virtualGuestStack = ExAllocatePoolWithTag(NonPagedPool, vCpus[j].virtualGuestStackSize, 'gstk');
-		RtlZeroMemory(vCpus[j].virtualGuestStack, vCpus[j].virtualGuestStackSize);
-		vCpus[j].virtualGuestStackBottom = (PVOID)((ULONG_PTR)vCpus[j].virtualGuestStack + vCpus[j].virtualGuestStackSize - 0x1000);
-
-		vCpus[j].virtualHostStackSize = 0x101000;
-		vCpus[j].virtualHostStack = ExAllocatePoolWithTag(NonPagedPool, vCpus[j].virtualHostStackSize, 'hstk');
-		RtlZeroMemory(vCpus[j].virtualHostStack, vCpus[j].virtualHostStackSize);
-		vCpus[j].virtualHostStackBottom = (PVOID)((ULONG_PTR)vCpus[j].virtualHostStack + vCpus[j].virtualHostStackSize - 0x1000);
-
-		*(ULONG*)vCpus[j].VMX_ON_REGION_VIRTUAL_KERNEL_ADDRESS = vmcsIdentifier;
-		*(ULONG*)vCpus[j].VMX_VMCS_REGION_VIRTUAL_KERNEL_ADDRESS = vmcsIdentifier;
-	}
+	SRA h_cs = { 0 }, h_ss = { 0 }, h_ds = { 0 }, h_es = { 0 }, h_fs = { 0 }, h_gs = { 0 }, h_ldtr = { 0 }, h_tr = { 0 };
+	SRA g_cs = { 0 }, g_ss = { 0 }, g_ds = { 0 }, g_es = { 0 }, g_fs = { 0 }, g_gs = { 0 }, g_ldtr = { 0 }, g_tr = { 0 };
+	
+	ULONG j = KeGetCurrentProcessorNumber();
+	KeSetSystemAffinityThread(1ull << j);
 	//开启CR4.VMXE位
-	for (size_t j = 0; j < totalCpuCount; j++)
-	{
-		KeSetSystemAffinityThread((KAFFINITY)(1 << j));
-		__vasm__setCR4VMXEBit();
-		vCpus[j].currentCr4 = __vsm__getCR4();
-		__vmx_on(&vCpus[j].VMX_ON_REGION_PHYSICAL_ADDRESS);
-	}
-	//选择一个虚拟CPU的VMCS作为`current VMCS`
-	KeSetSystemAffinityThread((KAFFINITY)(1 << currentVMCSCpuIndex));
-	__vmx_vmclear(&vCpus[currentVMCSCpuIndex].VMX_VMCS_REGION_PHYSICAL_ADDRESS);
-	__vmx_vmptrld(&vCpus[currentVMCSCpuIndex].VMX_VMCS_REGION_PHYSICAL_ADDRESS);
+	__vasm__setCR4VMXEBit();
+	vCpu[j].currentCr4 = __vsm__getCR4();
+	//Vmxon
+	__vmx_on(&vCpu[j].VMX_ON_REGION_PHYSICAL_ADDRESS);
+	//选择current VMCS
+	__vmx_vmclear(&vCpu[j].VMX_VMCS_REGION_PHYSICAL_ADDRESS);
+	__vmx_vmptrld(&vCpu[j].VMX_VMCS_REGION_PHYSICAL_ADDRESS);
 	//写入VMCS各个字段
 	/*1.VM-Exit Control字段*/
 	__vmx_vmwrite(VExCF_PRIMARY_VM_EXIT_CONTROLS, 0x36FFBul); //物理机 0x36FFB
@@ -261,12 +168,7 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING regPath)
 	__vmx_vmwrite(VECF_PIN_BASED_VM_EXECUTION_CONTROL, 0x16ul); //物理机 0x16ul
 	__vmx_vmwrite(VECF_PRIMARY_PROCESSOR_BASED_VM_EXECUTION_CONTROLS, 0x84006172ul); //物理机 0x4006172ul
 	__vmx_vmwrite(VECF_SECONDARY_PROCESSOR_BASED_VM_EXECUTION_CONTROLS, 0x101008ul); //物理机 0x101008ul
-	//PVOID msrBitmap = ExAllocatePoolWithTag(NonPagedPool, 0x3000, 'msrs');
-	//RtlZeroMemory(msrBitmap, 0x3000);
-	//PHYSICAL_ADDRESS msrBitmapPhy = MmGetPhysicalAddress(msrBitmap);
-	//__vmx_vmwrite(VECF_MSR_BITMAPS, msrBitmapPhy.QuadPart);
 	/*4.Host-State Area字段*/
-	SRA h_cs, h_ss, h_ds, h_es, h_fs, h_gs, h_ldtr, h_tr;
 	getSegementRegisterAttributes('cs', __vsm__getCS(), 0, &h_cs);
 	getSegementRegisterAttributes('ss', __vsm__getSS(), 0, &h_ss);
 	getSegementRegisterAttributes('ds', __vsm__getDS(), 0, &h_ds);
@@ -278,7 +180,7 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING regPath)
 	__vmx_vmwrite(HOST_CR0, __readcr0());
 	__vmx_vmwrite(HOST_CR3, __readcr3());
 	__vmx_vmwrite(HOST_CR4, __readcr4());
-	__vmx_vmwrite(HOST_RSP, (PVOID)vCpus[currentVMCSCpuIndex].virtualHostStackBottom);
+	__vmx_vmwrite(HOST_RSP, (PVOID)vCpu[j].virtualHostStackBottom);
 	__vmx_vmwrite(HOST_RIP, (PVOID)__vsm__hostEntry);
 	__vmx_vmwrite(HOST_CS_SELECTOR, h_cs.selector & 0xFFF8);
 	__vmx_vmwrite(HOST_SS_SELECTOR, h_ss.selector & 0xFFF8);
@@ -293,7 +195,6 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING regPath)
 	__vmx_vmwrite(HOST_GDTR_BASE_ADDRESS, (PVOID)__vsm__getGDTbase());
 	__vmx_vmwrite(HOST_IDTR_BASE_ADDRESS, (PVOID)__vsm__getIDTbase());
 	/*5.Guest-State Area字段*/
-	SRA g_cs = { 0 }, g_ss = { 0 }, g_ds = { 0 }, g_es = { 0 }, g_fs = { 0 }, g_gs = { 0 }, g_ldtr = { 0 }, g_tr = { 0 };
 	getSegementRegisterAttributes('cs', __vsm__getCS(), 1, &g_cs); //1
 	getSegementRegisterAttributes('ss', __vsm__getSS(), 1, &g_ss);
 	getSegementRegisterAttributes('ds', __vsm__getDS(), 1, &g_ds);
@@ -311,16 +212,16 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING regPath)
 	__vmx_vmwrite(GUEST_GS_BASE_ADDRESS, __readmsr(IA32_GS_BASE));
 	__vmx_vmwrite(GUEST_TR_BASE_ADDRESS, (PVOID)g_tr.baseAddress);
 	//限制
-	__vmx_vmwrite(GUEST_FS_SEGEMENT_LIMIT, g_fs.segementLimit); 
+	__vmx_vmwrite(GUEST_FS_SEGEMENT_LIMIT, g_fs.segementLimit & 0xFFFFul); //爹！
 	__vmx_vmwrite(GUEST_GS_SEGEMENT_LIMIT, g_gs.segementLimit);
 	__vmx_vmwrite(GUEST_TR_SEGEMENT_LIMIT, g_tr.segementLimit);
 	//权限
-	__vmx_vmwrite(GUEST_CS_ACCESS_RIGHTS, g_cs.accessRight & ~0xF00ull); 
-	__vmx_vmwrite(GUEST_SS_ACCESS_RIGHTS, g_ss.accessRight & ~0xF00ull); 
-	__vmx_vmwrite(GUEST_DS_ACCESS_RIGHTS, g_ds.accessRight & ~0xF00ull); 
-	__vmx_vmwrite(GUEST_ES_ACCESS_RIGHTS, g_es.accessRight & ~0xF00ull); 
-	__vmx_vmwrite(GUEST_FS_ACCESS_RIGHTS, g_fs.accessRight & ~0xF00ull); 
-	__vmx_vmwrite(GUEST_GS_ACCESS_RIGHTS, g_gs.accessRight & ~0xF00ull); 
+	__vmx_vmwrite(GUEST_CS_ACCESS_RIGHTS, g_cs.accessRight & ~0xF00ull);
+	__vmx_vmwrite(GUEST_SS_ACCESS_RIGHTS, g_ss.accessRight & ~0xF00ull);
+	__vmx_vmwrite(GUEST_DS_ACCESS_RIGHTS, g_ds.accessRight & ~0xF00ull);
+	__vmx_vmwrite(GUEST_ES_ACCESS_RIGHTS, g_es.accessRight & ~0xF00ull);
+	__vmx_vmwrite(GUEST_FS_ACCESS_RIGHTS, g_fs.accessRight & ~0xF00ull);
+	__vmx_vmwrite(GUEST_GS_ACCESS_RIGHTS, g_gs.accessRight & ~0xF00ull);
 	__vmx_vmwrite(GUEST_LDTR_ACCESS_RIGHTS, g_ldtr.accessRight & ~0xF00ull);
 	__vmx_vmwrite(GUEST_TR_ACCESS_RIGHTS, g_tr.accessRight & ~0xF00ull);
 	//G/IDT表
@@ -336,10 +237,80 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING regPath)
 	__vmx_vmwrite(GUEST_CR4, __readcr4());
 	__vmx_vmwrite(GUEST_DR7, __vsm__getDR7());
 	__vmx_vmwrite(GUEST_RFLAGS, __readeflags());
-	__vmx_vmwrite(GUEST_RSP, (PVOID)vCpus[currentVMCSCpuIndex].virtualGuestStackBottom);
+	__vmx_vmwrite(GUEST_RSP, (PVOID)vCpu[j].virtualGuestStackBottom);
 	__vmx_vmwrite(GUEST_RIP, (PVOID)__vsm__guestEntry);
 
 	__vsm__vmlaunchSaveRegisters();
 
-	return STATUS_SUCCESS;	
+	return 0;
+}
+
+VOID driverUnload(PDRIVER_OBJECT driverObject)
+{
+	UNREFERENCED_PARAMETER(driverObject);
+	KeSetSystemAffinityThread((KAFFINITY)(1 << 0));
+	__vsm__vmxoffSaveRegisters();
+
+	ExFreePool(vCpu->virtualGuestStack);
+	vCpu->virtualGuestStack = NULL;
+	ExFreePool(vCpu->virtualHostStack);
+	vCpu->virtualHostStack = NULL;
+	ExFreePool(vCpu->VMX_VMCS_REGION_VIRTUAL_KERNEL_ADDRESS);
+	vCpu->VMX_VMCS_REGION_VIRTUAL_KERNEL_ADDRESS = NULL;
+	ExFreePool(vCpu->VMX_ON_REGION_VIRTUAL_KERNEL_ADDRESS);
+	vCpu->VMX_ON_REGION_VIRTUAL_KERNEL_ADDRESS = NULL;
+	ExFreePool(vCpu);
+	vCpu = NULL;
+
+	return;
+}
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING regPath)
+{
+	UNREFERENCED_PARAMETER(regPath);
+	bugCheck = (ULONG_PTR)KeBugCheckEx;
+	driverObject->DriverUnload = driverUnload;
+
+	totalCpuCount = KeQueryActiveProcessorCount(NULL);
+	PER_CPU_REGS = (ULONG_PTR*)ExAllocatePoolWithTag(NonPagedPool, totalCpuCount * sizeof(ULONG_PTR), 'zzaa');
+	
+	vCpu = (PVCPU)ExAllocatePoolWithTag(NonPagedPool, totalCpuCount * sizeof(VCPU), 'zzaa');
+	RtlZeroMemory(vCpu, totalCpuCount * sizeof(VCPU));
+
+	SIZE_T regionSizeNeeded = 0x1000;
+	ULONG vmcsIdentifier = (ULONG)4;
+	PHYSICAL_ADDRESS tempVmxon = { 0 };
+	PHYSICAL_ADDRESS tempVmcsRegion = { 0 };
+	
+	for (size_t j = 0; j < totalCpuCount; j++)
+	{
+		vCpu[j].VMX_ON_REGION_VIRTUAL_KERNEL_ADDRESS = ExAllocatePoolWithTag(NonPagedPool, regionSizeNeeded, 'zzaa');
+		RtlZeroMemory(vCpu[j].VMX_ON_REGION_VIRTUAL_KERNEL_ADDRESS, regionSizeNeeded);
+		tempVmxon = MmGetPhysicalAddress(vCpu[j].VMX_ON_REGION_VIRTUAL_KERNEL_ADDRESS);
+		vCpu[j].VMX_ON_REGION_PHYSICAL_ADDRESS = tempVmxon.QuadPart;
+
+		vCpu[j].VMX_VMCS_REGION_VIRTUAL_KERNEL_ADDRESS = ExAllocatePoolWithTag(NonPagedPool, regionSizeNeeded, 'zzaa');
+		RtlZeroMemory(vCpu[j].VMX_VMCS_REGION_VIRTUAL_KERNEL_ADDRESS, regionSizeNeeded);
+		tempVmcsRegion = MmGetPhysicalAddress(vCpu[j].VMX_VMCS_REGION_VIRTUAL_KERNEL_ADDRESS);
+		vCpu[j].VMX_VMCS_REGION_PHYSICAL_ADDRESS = tempVmcsRegion.QuadPart;
+
+		vCpu[j].virtualGuestStackSize = 0x11000;
+		vCpu[j].virtualGuestStack = ExAllocatePoolWithTag(NonPagedPool, vCpu[j].virtualGuestStackSize, 'zzaa');
+		RtlZeroMemory(vCpu[j].virtualGuestStack, vCpu[j].virtualGuestStackSize);
+		vCpu[j].virtualGuestStackBottom = (PVOID)((ULONG_PTR)vCpu[j].virtualGuestStack + vCpu[j].virtualGuestStackSize - 0x1000);
+
+		vCpu[j].virtualHostStackSize = 0x11000;
+		vCpu[j].virtualHostStack = ExAllocatePoolWithTag(NonPagedPool, vCpu[j].virtualHostStackSize, 'zzaa');
+		RtlZeroMemory(vCpu[j].virtualHostStack, vCpu[j].virtualHostStackSize);
+		vCpu[j].virtualHostStackBottom = (PVOID)((ULONG_PTR)vCpu[j].virtualHostStack + vCpu[j].virtualHostStackSize - 0x1000);
+
+		PER_CPU_REGS[j] = (ULONG_PTR)vCpu[j].regs;
+
+		*(ULONG*)vCpu[j].VMX_ON_REGION_VIRTUAL_KERNEL_ADDRESS = vmcsIdentifier;
+		*(ULONG*)vCpu[j].VMX_VMCS_REGION_VIRTUAL_KERNEL_ADDRESS = vmcsIdentifier;
+	}	
+
+	KeIpiGenericCall(v, 0);
+
+	return STATUS_SUCCESS;
 }
